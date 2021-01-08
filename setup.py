e1784300b3313f7af83cee9e8a254eb29605bbc6
Jorge <jbernal0019@yahoo.es>
2021-01-08 10:23:37 -0500

    First implementation of client module

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
-import sys
 
-# Make sure we are running python3.5+
-if 10 * sys.version_info[0] + sys.version_info[1] < 35:
-    sys.exit("Sorry, only Python 3.5+ is supported.")
-
+from os import path
 from setuptools import setup
 
-def readme():
-    with open('README.rst') as f:
-        return f.read()
+with open(path.join(path.dirname(path.abspath(__file__)), 'README.rst')) as f:
+    readme = f.read()
 
 setup(
       name             =   'python-pfconclient',
       version          =   '0.0.1',
       description      =   '(Python) client for the Pfcon API',
-      long_description =   readme(),
+      long_description =   readme,
       author           =   'FNNDSC',
       author_email     =   'dev@babymri.org',
       url              =   'https://github.com/FNNDSC/python-pfconclient',
@@ -24,5 +19,6 @@ setup(
       tests_require    =   ['nose'],
       scripts          =   ['bin/pfconclient'],
       license          =   'MIT',
-      zip_safe=False
+      zip_safe         =   False,
+      python_requires  =   '>=3.7'
 )
